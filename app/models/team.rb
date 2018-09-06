19b4648e19721d30e8c91120d6d92a1f0ac3628e
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-06 15:55:46 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,16 @@
 class Team
   attr_reader :name, :motto
+  
   @@all = []
+  
   def initialize(params)
     @name = params[:team][:name]
     @motto = params[:team][:motto]
     @@all << self
   end
+  
   def self.all
     @@all
   end
+  
 end
