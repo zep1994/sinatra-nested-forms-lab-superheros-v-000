1ad776414e97c4deeabc82b54c0a200f5783253f
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-06 15:55:37 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,13 +1,17 @@
 class Superhero
   attr_reader :name, :power, :bio
+  
   @@all = []
+  
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
     @@all << self
   end
+  
   def self.all
     @@all
   end
+  
 end
