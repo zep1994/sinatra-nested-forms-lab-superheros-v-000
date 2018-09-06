df02cb5003a948b53b532402b5f8b6d95d8eb8d4
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-06 15:55:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,15 @@ class App < Sinatra::Base
      get '/' do
       erb :super_hero
     end
+    
     post '/teams' do
       @team = Team.new(params)
       params[:team][:members].each do |member_details|
         superhero = Superhero.new(member_details)
       end
+      
       @superheros = Superhero.all
       erb :team
     end
+    
 end
